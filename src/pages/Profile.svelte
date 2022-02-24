2f7b89c1876352f3ac3170edf2adfa1ed7f1b259
yajatvishwak <yajat472001@gmail.com>
2022-02-24 12:43:47 +0530

    profile

---
+++ src/pages/Profile.svelte
@@ -0,0 +1,14 @@
+<script>
+    import NavBar from "../components/NavBar.svelte";
+    import Heading from "../components/Heading.svelte";
+    import BoundingBox from "../components/BoundingBox.svelte";
+
+    // export let name;
+    // import StoryCard from "./StoryCard.svelte";
+</script>
+
+<BoundingBox>
+    <NavBar />
+    <Heading>Profile Settings ⚙️</Heading>
+    <div class="my-10 gap-5 grid md:grid-cols-2" />
+</BoundingBox>
--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import Home from "./pages/Home.svelte"
 import AddContribution from "./pages/AddContribution.svelte"
 import Contributors from "./pages/Contributors.svelte"
 import NotFound from "./pages/NotFound.svelte"
+import Profile from "./pages/Profile.svelte"
 const routes = {
     // Exact path
     '/': Home,
     "/contributors": Contributors,
     "/add": AddContribution,
+    "/profile": Profile,
     "*" : NotFound
     
 }
